9b5538f8f2d4839ef97755824bf359a65fe3b041
KK <Home@KKs-MacBook-Pro.local>
2026-03-06 10:34:59 +0800

    Initial commit: KU Studio Launch

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./KUStudio.jsx",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
